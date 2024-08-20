30d401b5a4ad8f8cb36f65da1f1e88c45ac6a7f5
Alex <150475303+hpa123@users.noreply.github.com>
2024-08-20 13:58:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from graphics import Window
 from maze import Maze
 
 def main():
+    # defines a seed for random number generation; mostly for testing purposes, so that
+    # the solver behaves the same way each run.
     seed = None
 
     win = Window()
